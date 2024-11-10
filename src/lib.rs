6e3e89bf00792c78e8ab472c7df8b88ce2f7d8a2
Hantong Chen <cxwdyx620@gmail.com>
2024-11-10 20:12:47 +0800

    feat: introduce more choice on br file size

--- src/lib.rs
+++ src/lib.rs
@@ -2,10 +2,16 @@
 
 use worker::*;
 
+/// Brotli Data: 50MB
+static BR_50M_BYTE: &[u8] = include_bytes!("../assets/50mb.test");
 /// Brotli Data: 100MB
 static BR_100M_BYTE: &[u8] = include_bytes!("../assets/100mb.test");
 /// Brotli Data: 200MB
 static BR_200M_BYTE: &[u8] = include_bytes!("../assets/200mb.test");
+/// Brotli Data: 300MB
+static BR_300M_BYTE: &[u8] = include_bytes!("../assets/300mb.test");
+/// Brotli Data: 500MB
+static BR_500M_BYTE: &[u8] = include_bytes!("../assets/500mb.test");
 /// Brotli Data: 1000MB
 static BR_1G_BYTE: &[u8] = include_bytes!("../assets/1gb.test");
 /// Server version
@@ -18,8 +24,11 @@ async fn fetch(req: Request, _env: Env, _ctx: Context) -> Result<Response> {
     console_debug!("accept request: {:#?}", &req);
 
     let body = match req.path().as_str() {
+        "/50mb.test" => BR_50M_BYTE,
         "/100mb.test" => BR_100M_BYTE,
         "/200mb.test" => BR_200M_BYTE,
+        "/300mb.test" => BR_300M_BYTE,
+        "/500mb.test" => BR_500M_BYTE,
         "/1gb.test" => BR_1G_BYTE,
         _ => {
             console_warn!("unknown request path");
